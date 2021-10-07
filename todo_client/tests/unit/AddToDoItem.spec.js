0753ee2733a8d99ad0aff8c869970f197df869af
Sinan <sinanabus@sabanciuniv.edu>
2021-10-07 19:33:53 +0300

    Coded tests for rendering the button and checking it's name RED

--- todo_client/tests/unit/AddToDoItem.spec.js
+++ todo_client/tests/unit/AddToDoItem.spec.js
@@ -1,28 +1,26 @@
 import { shallowMount } from '@vue/test-utils'
 import AddToDoItem from '@/components/AddToDoItem.vue'
 
+const wrapper = shallowMount (AddToDoItem);
+
 describe('AddToDoItem.vue', () =>  {
     test('renders an input box', () => {
-        const empty = '';
-        const wrapper = shallowMount (AddToDoItem, {
-            props : {empty}
-         });
     expect(wrapper.exists()).toBe(true);
     });
-    
+
     test('renders an input box', () => {
-        const empty = '';
-        const wrapper = shallowMount (AddToDoItem, {
-            props : {empty}
-    });
     expect(wrapper.find('#ToDoInput').exists()).toBe(true);
     });
     
     test('input placeholder should be Create a new todo', () => {
-        const empty = '';
-        const wrapper = shallowMount (AddToDoItem, {
-            props : {empty}
-        });
         expect(wrapper.find('#ToDoInput').attributes()['placeholder']).toBe("Create a new todo");
     });
+    
+    test("renders an button." , () => {
+        expect(wrapper.find('#AddButton').exists()).toBe(true);
+    });
+    
+    test("button text should be Add", () => {
+        expect(wrapper.find("#AddButton").text()).toBe(true);
+    })
 });
