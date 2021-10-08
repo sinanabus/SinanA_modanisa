f89704fbb9a507a69a8e6d155d974556ec43e962
Sinan <sinanabus@sabanciuniv.edu>
2021-10-08 09:14:10 +0300

    Added acceptance criteria to read first todo in ToDoList - RED

--- acceptance_test/specs/test.spec
+++ acceptance_test/specs/test.spec
@@ -1,5 +1,8 @@
 # Welcome to Modanisa Assignment Acceptance
 ## Connect to Local Host
 * Go to local server
-* Add "first todo"
+* Test header contains To Do App
+## Write "first todo", Click Add Button and Read "first to do"
+* Write "first todo"
 * Click AddButton
+* ToDoList contains "first todo"
--- acceptance_test/tests/test.js
+++ acceptance_test/tests/test.js
@@ -1,4 +1,5 @@
-const { openBrowser, closeBrowser, write, into, goto, textBox, click, button } = require('taiko');
+const { openBrowser, closeBrowser, write, into, goto, textBox, click, button, text, listItem } = require('taiko');
+const assert = require("assert");
 const headless = process.env.headless_chrome.toLowerCase() === 'true';
 
 
@@ -16,11 +17,20 @@ step("Go to local server", async () => {
     await goto("http://localhost:8080/")
 });
 
-step("Add <item>", async (item) => {
+step("Test header contains To Do App", async () => {
+    assert.ok(await text("To Do App").exists());
+}
+);
+
+step("Write <item>", async (item) => {
     await write(item, into(textBox("Create a new todo")));
 });
 
 step("Click AddButton", async () => {
     await click(button('Add'));
+});
+
+step("ToDoList contains <item>", async (item) => {
+    assert.ok(await listItem(item).exists());
 });
 
