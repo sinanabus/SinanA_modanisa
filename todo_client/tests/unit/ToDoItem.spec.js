b506f99dedd7763f08d4d7ec5035cea6cca5816d
Sinan <sinanabus@sabanciuniv.edu>
2021-10-08 09:47:03 +0300

    Coded ToDoItem.spec.js for testing ToDoItem component

---
+++ todo_client/tests/unit/ToDoItem.spec.js
@@ -0,0 +1,19 @@
+import { shallowMount } from '@vue/test-utils'
+import ToDoItem from '@/components/ToDoItem.vue'
+
+const text = "To do item text 1";
+const wrapper = shallowMount (ToDoItem , {
+    propsData: {
+        input: { text: text}
+    }
+});
+
+describe('ToDoItem.vue', () => {
+    test('renders a ToDoItem vue component', () => {
+        expect(wrapper.exists()).toBe(true);
+    })
+
+    test('ToDoItem component contains expected text', () => {
+        expect(wrapper.text()).toBe(text)
+    });
+});
